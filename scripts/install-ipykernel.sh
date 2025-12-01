88b750761145060df7b00d5b6b62bc78e504ad9e
Alex <ayaroslavskiy91@gmail.com>
2025-12-01 08:40:55 -0500

    initial commit

---
+++ scripts/install-ipykernel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S uv run bash
+
+python -m ipykernel install --user \
+  --name=advent_of_code \
+  --display-name='Python 3.12 / advent-of-code'
---
+++ src/advent_of_code/__init__.py
@@ -0,0 +1,7 @@
+"""
+advent-of-code
+
+Repository for all future advent of code submissions, starting from 2025
+"""
+
+from .__about__ import __version__ as __version__
